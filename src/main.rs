b45fa29d98edcf7907f110ff9b9ed93a0a67a48a
Mordecai Malignatus <mordecai@malignat.us>
2021-11-10 15:36:08 +0100

    Chore: Clippy refactorings.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::pedantic, clippy::all)]
+
 use ansi_term::Style;
 use chrono::prelude::*;
 use clap::{App, AppSettings, Arg};
@@ -16,7 +18,53 @@ use std::process::Command;
 type M = GenericDecimal<u64, u8>;
 
 fn main() -> Result<()> {
-    let args = App::new("sq")
+    let args = parse_args();
+    match args.subcommand() {
+        ("budget", Some(m)) => {
+            // This is a bug waiting to happen, I think. Needs to be converted
+            // to pass Options to `update_budget`.
+            let income = m
+                .value_of("amount")
+                .unwrap()
+                .parse()
+                .expect("can't parse amount");
+            let interval = m
+                .value_of("interval")
+                .unwrap()
+                .parse()
+                .expect("can't parse interval");
+
+            update_budget(income, interval)?;
+        }
+        ("buy", Some(m)) => {
+            let no_open = m.is_present("no-open");
+            let new_price = m
+                .value_of("new_price")
+                .map(|s| s.parse::<f64>().expect("Can't parse specified new price"))
+                .map(M::from);
+            buy_item(no_open, new_price)?;
+        }
+        ("list", _) => display_queue(),
+        ("delete", _) => delete_head_item()?,
+        ("past", _) => display_prior_purchases(),
+        ("bump", _) => bump_head()?,
+        ("add", Some(m)) => {
+            let prepend = m.is_present("prepend");
+            let to_add = m
+                .values_of("words")
+                .unwrap()
+                .collect::<Vec<&str>>()
+                .join(" ");
+            add_to_purchase_queue(to_add, prepend)?;
+        }
+        _ => display_status()?,
+    }
+
+    Ok(())
+}
+
+fn parse_args() -> clap::ArgMatches<'static> {
+    App::new("sq")
         .version("0.1")
         .author("Mordecai Malignatus <mordecai@malignat.us>")
         .about("The tiniest spending queue")
@@ -80,49 +128,7 @@ fn main() -> Result<()> {
                 )
                 .arg(Arg::from_usage("<words>... 'Description of thing to buy'")),
         )
-        .get_matches();
-
-    match args.subcommand() {
-        ("status", _) => display_status(),
-        ("budget", Some(m)) => {
-            // This is a bug waiting to happen, I think. Needs to be converted
-            // to pass Options to `update_budget`.
-            let income = m
-                .value_of("amount")
-                .unwrap()
-                .parse()
-                .expect("can't parse amount");
-            let interval = m
-                .value_of("interval")
-                .unwrap()
-                .parse()
-                .expect("can't parse interval");
-
-            update_budget(income, interval)
-        }
-        ("buy", Some(m)) => {
-            let no_open = m.is_present("no-open");
-            let new_price = m
-                .value_of("new_price")
-                .map(|s| s.parse::<f64>().expect("Can't parse specified new price"))
-                .map(M::from);
-            buy_item(no_open, new_price)
-        }
-        ("list", _) => display_queue(),
-        ("delete", _) => delete_head_item(),
-        ("past", _) => display_prior_purchases(),
-        ("bump", _) => bump_head(),
-        ("add", Some(m)) => {
-            let prepend = m.is_present("prepend");
-            let to_add = m
-                .values_of("words")
-                .unwrap()
-                .collect::<Vec<&str>>()
-                .join(" ");
-            add_to_purchase_queue(to_add, prepend)
-        }
-        _ => display_status(),
-    }
+        .get_matches()
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -136,7 +142,7 @@ struct Item {
 #[derive(Serialize, Deserialize, Debug)]
 struct Income {
     amount: f64,
-    interval_in_days: u8,
+    interval_in_days: u64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -172,7 +178,7 @@ fn buy_item(suppress_opening_url: bool, new_price: Option<M>) -> Result<()> {
                 );
 
                 if !suppress_opening_url {
-                    open_url(&item.purchase_link.clone())?
+                    open_url(&item.purchase_link.clone())?;
                 }
 
                 state.current_amount -= cost;
@@ -187,7 +193,7 @@ fn buy_item(suppress_opening_url: bool, new_price: Option<M>) -> Result<()> {
         }
     }
 
-    write_file(state)
+    write_file(&state)
 }
 
 /// Move current head of queue back 1-3 spots. This is essentially a "not right
@@ -213,7 +219,7 @@ fn bump_head() -> Result<()> {
                 bold.paint((new_position + 1).to_string()),
                 bold.paint(state.future_purchases.front().unwrap().name.clone())
             );
-            write_file(state)?;
+            write_file(&state)?;
             display_status()?;
         }
     }
@@ -223,16 +229,13 @@ fn bump_head() -> Result<()> {
 
 fn delete_head_item() -> Result<()> {
     let mut state = read_file();
-    match state.future_purchases.pop_front() {
-        Some(item) => {
-            println!("Deleted item at head of queue: {}", item.name);
-            write_file(state)?;
-            display_status()
-        }
-        None => {
-            eprintln!("No item in queue, can't remove any.");
-            Ok(())
-        }
+    if let Some(item) = state.future_purchases.pop_front() {
+        println!("Deleted item at head of queue: {}", item.name);
+        write_file(&state)?;
+        display_status()
+    } else {
+        eprintln!("No item in queue, can't remove any.");
+        Ok(())
     }
 }
 
@@ -255,22 +258,21 @@ fn open_url(url: &Option<String>) -> Result<()> {
 }
 
 /// Print the list as it is right now.
-fn display_queue() -> Result<()> {
+fn display_queue() {
     let state = read_file();
     state.future_purchases.iter().for_each(|item| {
         let cost = format!("${:#.2}", item.amount);
-        let name = match item.purchase_link.is_some() {
-            true => Style::new().italic().paint(item.name.clone()),
-            false => item.name.clone().into(),
+        let name = if item.purchase_link.is_some() {
+            Style::new().italic().paint(item.name.clone())
+        } else {
+            item.name.clone().into()
         };
         println!("{}\t{}", name, cost);
     });
-
-    Ok(())
 }
 
 /// Print list of past purchases, the things already bought.
-fn display_prior_purchases() -> Result<()> {
+fn display_prior_purchases() {
     let state = read_file();
     state.past_purchases.iter().for_each(|item| {
         let cost = format!("${:#.2}", item.amount);
@@ -280,8 +282,6 @@ fn display_prior_purchases() -> Result<()> {
         };
         println!("{}\t{}\t{}", item.name, cost, ts);
     });
-
-    Ok(())
 }
 
 fn parse_float_from_stdin(prompt: &str) -> f64 {
@@ -316,8 +316,8 @@ fn add_to_purchase_queue(thing_to_add: String, prepend: bool) -> Result<()> {
     let mut purchase_url = String::new();
     match stdin.read_line(&mut purchase_url) {
         Ok(_bytes_read) => {}
-        Err(_) => {
-            panic!("Can't parse purchase url, aborting...");
+        Err(x) => {
+            panic!("Can't parse purchase url because {}, aborting...", x);
         }
     }
     purchase_url = purchase_url.trim().to_string();
@@ -342,7 +342,7 @@ fn add_to_purchase_queue(thing_to_add: String, prepend: bool) -> Result<()> {
         state.future_purchases.push_back(item);
     }
 
-    write_file(state)
+    write_file(&state)
 }
 
 fn display_status() -> Result<()> {
@@ -373,14 +373,14 @@ fn display_status() -> Result<()> {
                 bold.paint(&amount)
             );
             if state.current_amount >= item.amount {
-                println!("{}", bold.paint("*** NEXT ITEM PURCHASEABLE ***"))
+                println!("{}", bold.paint("*** NEXT ITEM PURCHASEABLE ***"));
             }
         }
         None => println!("There's no next item in the queue, add one!"),
     };
 
     println!();
-    write_file(state)
+    write_file(&state)
 }
 
 fn calculate_current_amount(state: &State) -> (DateTime<Local>, M) {
@@ -388,13 +388,13 @@ fn calculate_current_amount(state: &State) -> (DateTime<Local>, M) {
     let then = DateTime::parse_from_rfc2822(&state.last_calculation)
         .expect("Can't parse date from last calculation, check the statefile");
     let time_between = now.signed_duration_since(then).num_seconds();
-    let time_between = M::from(time_between as u64);
+    let time_between = M::from(time_between);
     let income = M::from(state.income.amount);
 
     let interval = M::from(state.income.interval_in_days as u64);
     let current_balance = state.current_amount;
 
-    let seconds_in_interval = M::from(24u64 * 60 * 60) * interval;
+    let seconds_in_interval = M::from(24_u64 * 60 * 60) * interval;
     let money_per_second = income / seconds_in_interval;
     let since_last_calc = time_between * money_per_second;
 
@@ -403,7 +403,7 @@ fn calculate_current_amount(state: &State) -> (DateTime<Local>, M) {
     (now, subtotal)
 }
 
-fn update_budget(amount: f64, interval: u8) -> Result<()> {
+fn update_budget(amount: f64, interval: u64) -> Result<()> {
     let mut state = read_file();
 
     println!("Updated income to ${:.2} per {} days.", amount, interval);
@@ -411,7 +411,7 @@ fn update_budget(amount: f64, interval: u8) -> Result<()> {
         amount,
         interval_in_days: interval,
     };
-    write_file(state)
+    write_file(&state)
 }
 
 fn file() -> PathBuf {
@@ -424,39 +424,35 @@ fn file() -> PathBuf {
     home
 }
 
-// TODO: This could be a lot better
 fn read_file() -> State {
     let statepath = file();
 
     if !statepath.exists() {
         let mut t = file();
         let _ = t.pop();
-        std::fs::create_dir_all(t).expect("can't create config dir")
+        std::fs::create_dir_all(t).expect("can't create config dir");
     }
 
-    match fs::read_to_string(statepath) {
-        Ok(string) => {
-            serde_json::from_str(&string).expect("Can't parse statefile, check the formatting")
-        }
-        Err(_) => {
-            eprintln!("Can't read statefile, continuing with default");
-            eprintln!("You're going to want to adjust the income, currently $1/mo.");
-            State {
-                income: Income {
-                    amount: 1.0,
-                    interval_in_days: 30,
-                },
-                current_amount: M::from(0),
-                last_calculation: Local::now().to_rfc2822(),
-                future_purchases: VecDeque::new(),
-                past_purchases: VecDeque::new(),
-            }
+    if let Ok(string) = fs::read_to_string(statepath) {
+        serde_json::from_str(&string).expect("Can't parse statefile, check the formatting")
+    } else {
+        eprintln!("Can't read statefile, continuing with default");
+        eprintln!("You're going to want to adjust the income, currently $1/mo.");
+        State {
+            income: Income {
+                amount: 1.0,
+                interval_in_days: 30,
+            },
+            current_amount: M::from(0),
+            last_calculation: Local::now().to_rfc2822(),
+            future_purchases: VecDeque::new(),
+            past_purchases: VecDeque::new(),
         }
     }
 }
 
-fn write_file(state: State) -> Result<()> {
-    fs::write(file(), serde_json::to_string_pretty(&state).unwrap())
+fn write_file(state: &State) -> Result<()> {
+    fs::write(file(), serde_json::to_string_pretty(state).unwrap())
 }
 
 #[cfg(test)]
